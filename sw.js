03637f2f8f4b59387c94af647c28ecf04dc06f17
D3SOX <nico@d3sox.me>
2025-11-30 10:31:06 +0100

    Initial commit: Add substance injector webapp

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'substance-injector-v2';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  // Don't skipWaiting here - wait for user to click "Update"
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) => 
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetched = fetch(e.request).then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
+
+// Listen for skip waiting message from the page
+self.addEventListener('message', (e) => {
+  if (e.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
